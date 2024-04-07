5205cf3b02ec49ca482c760e9ffa2d87217684d2
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-07 22:46:46 +0800

    Convert docx file to markdown with page number.

--- quickstarts/UnitTests/AsposeNet/AsposeWordsTest.cs
+++ quickstarts/UnitTests/AsposeNet/AsposeWordsTest.cs
@@ -1,6 +1,7 @@
 ﻿using Aspose.Words;
 using Aspose.Words.Saving;
 using QuickStart;
+using System.Text;
 
 namespace UnitTests.AsposeNet;
 
@@ -31,5 +32,41 @@ public class AsposeWordsTest(ITestOutputHelper output) : BaseTest(output)
         Document doc = new(Path.Join(wordDirectory, "test.docx"));
 
         doc.Save(Path.Join(wordDirectory, "test.pdf"));
+    }
+
+    [Fact]
+    public void RunConvertToMarkwonWithPageNumber()
+    {
+        StringBuilder markdownContentBuilder = new();
+
+        string wordDirectory = Path.Join(AppContext.BaseDirectory, "docs", "word");
+
+        Document doc = new(Path.Join(wordDirectory, "test.docx"));
+
+        for (int i = 0; i < doc.PageCount; i++)
+        {
+            Document pageDocument = doc.ExtractPages(i, 1);
+
+            MarkdownSaveOptions saveOptions = new()
+            {
+                ForcePageBreaks = true,
+                ImagesFolderAlias = $"./imgs/{i}/",
+                ImagesFolder = Path.Combine(wordDirectory, "imgs", i.ToString())
+            };
+
+            string markdownSavePath = Path.Join(wordDirectory, $"test-{i}.md");
+
+            pageDocument.Save(markdownSavePath, saveOptions);
+
+            string markdownContent = File.ReadAllText(markdownSavePath);
+
+            markdownContentBuilder.AppendLine(markdownContent);
+
+            markdownContentBuilder.AppendLine($"<!-- 转换后的Markdown的页码标识符，当前是第{i + 1}页 -->");
+
+            File.Delete(markdownSavePath);
+        }
+
+        File.WriteAllText(Path.Join(wordDirectory, "test.md"), markdownContentBuilder.ToString(), Encoding.UTF8);
     }
 }
