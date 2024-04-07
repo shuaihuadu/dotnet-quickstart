10bf634c634de3bafcffeec67e49b3040a6ed2a3
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-07 22:11:06 +0800

    Add Aspose tests.

---
+++ quickstarts/UnitTests/AsposeNet/AsposePdfTests.cs
@@ -0,0 +1,22 @@
+﻿using Aspose.Pdf;
+
+namespace UnitTests.AsposeNet;
+
+public class AsposePdfTests
+{
+    [Fact]
+    public void RunConvertToMarkdown()
+    {
+        // 先将pdf转换为word
+        // 再将word转换为markdown
+        string wordDirectory = Path.Join(AppContext.BaseDirectory, "docs", "pdf");
+
+        Document doc = new(Path.Join(wordDirectory, "test.pdf"));
+
+        doc.Save("test.docx", SaveFormat.Doc);
+
+        var outputDocument = new Aspose.Words.Document(Path.Join(AppContext.BaseDirectory, "test.docx"));
+
+        outputDocument.Save(Path.Join(wordDirectory, "test.md"));
+    }
+}
---
+++ quickstarts/UnitTests/AsposeNet/AsposeWordsTest.cs
@@ -0,0 +1,35 @@
+﻿using Aspose.Words;
+using Aspose.Words.Saving;
+using QuickStart;
+
+namespace UnitTests.AsposeNet;
+
+public class AsposeWordsTest(ITestOutputHelper output) : BaseTest(output)
+{
+    [Fact]
+    public void RunConvertToMarkdown()
+    {
+        string wordDirectory = Path.Join(AppContext.BaseDirectory, "docs", "word");
+
+        Document doc = new(Path.Join(wordDirectory, "test.docx"));
+
+        MarkdownSaveOptions saveOptions = new()
+        {
+            ForcePageBreaks = true,
+            ImagesFolderAlias = "./imgs/",
+            ImagesFolder = Path.Combine(wordDirectory, "imgs")
+        };
+
+        doc.Save(Path.Join(wordDirectory, "test.md"), saveOptions);
+    }
+
+    [Fact]
+    public void RunConvertToPdf()
+    {
+        string wordDirectory = Path.Join(AppContext.BaseDirectory, "docs", "word");
+
+        Document doc = new(Path.Join(wordDirectory, "test.docx"));
+
+        doc.Save(Path.Join(wordDirectory, "test.pdf"));
+    }
+}
