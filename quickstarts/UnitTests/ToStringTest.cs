bb72c9115fbf7361fb5175ac3cd93cde423b80a3
ShuaiHua Du <shuaihua.du@outlook.com>
2024-04-13 11:20:45 +0800

    Add tests.

---
+++ quickstarts/UnitTests/ToStringTest.cs
@@ -0,0 +1,13 @@
+﻿using QuickStart;
+using System.Globalization;
+
+namespace UnitTests;
+
+public class ToStringTest(ITestOutputHelper output) : BaseTest(output)
+{
+    [Fact]
+    public void ToStringWithNumberFormatInfoTest()
+    {
+        WriteLine(3.ToString(new NumberFormatInfo()));
+    }
+}
