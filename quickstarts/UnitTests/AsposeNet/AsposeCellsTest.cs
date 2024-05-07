fa4718b3f4bbc02ec12b77bb9eb2dae57ac28ee1
ShuaiHua Du <shuaihua.du@outlook.com>
2024-05-07 18:14:16 +0800

    Add aspose cells test.

---
+++ quickstarts/UnitTests/AsposeNet/AsposeCellsTest.cs
@@ -0,0 +1,20 @@
+﻿using Aspose.Cells;
+
+namespace UnitTests.AsposeNet;
+
+public class AsposeCellsTest
+{
+    [Fact]
+    public void RunConvertToWord()
+    {
+        string xlsx = Path.Join(AppContext.BaseDirectory, "docs", "xlsx", "1.xlsx");
+
+        LoadOptions loadOptions = new();
+
+        loadOptions.SetPaperSize(PaperSizeType.PaperA4);
+        loadOptions.IgnoreNotPrinted = true;
+
+        var workbook = new Workbook(xlsx, loadOptions);
+        workbook.Save("1.docx");
+    }
+}
