7d260d59df6a6b4b4323945e49fb4019a3f9d6a8
ShuaiHua Du <shuaihua.du@outlook.com>
2024-07-05 00:18:50 +0800

    Add postgres tests.

---
+++ quickstarts/UnitTests/Postgres/Postgres_Test.cs
@@ -0,0 +1,29 @@
+﻿using Npgsql;
+
+namespace UnitTests.Postgres;
+
+public class Postgres_Test(ITestOutputHelper output) : BaseTest(output)
+{
+    [Fact]
+    public async Task TestConnection()
+    {
+        var connectionString = "Host=localhost;UserName=postgres;Password=p@ssword;Database=Test";
+
+        // 创建连接对象  
+        using (var conn = new NpgsqlConnection(connectionString))
+        {
+            // 打开连接  
+            conn.Open();
+
+            // 执行查询  
+            using (var cmd = new NpgsqlCommand("SELECT * FROM items", conn))
+            using (var reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(0));
+                }
+            }
+        }
+    }
+}
