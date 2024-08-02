2502666a65ae2d53abb69a9f438f4ff6caf6c4f7
ShuaiHua Du <shuaihua.du@outlook.com>
2024-08-03 00:03:36 +0800

    Add new pdf creation tests.

--- quickstarts/UnitTests/PDF/iTextWritePdfTest.cs
+++ quickstarts/UnitTests/PDF/iTextWritePdfTest.cs
@@ -4,59 +4,18 @@ using iText.Kernel.Font;
 using iText.Kernel.Pdf;
 using iText.Layout;
 using iText.Layout.Element;
-using iText.Layout.Properties;
 
 namespace UnitTests.iText;
 
 public class iTextWritePdfTest
 {
     [Fact]
-    public void A()
-    {
-        byte[] imageContent = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "PDF", "sk.png"));
-
-        // 设置输出PDF文件路径  
-        string dest = "output.pdf";
-
-        // 创建PDF Writer实例  
-        PdfWriter writer = new PdfWriter(dest);
-
-        // 创建PDF Document实例  
-        PdfDocument pdf = new PdfDocument(writer);
-
-        // 创建Document实例  
-        Document document = new Document(pdf);
-
-        // 添加一个段落文字  
-        Paragraph paragraph = new Paragraph("Hello, iText!")
-            .SetTextAlignment(TextAlignment.CENTER)
-            .SetFontSize(20);
-        document.Add(paragraph);
-
-        // 添加图片  
-        ImageData imageData = ImageDataFactory.Create(imageContent);
-        Image img = new Image(imageData);
-
-        // 设置图片大小和位置（可选）  
-        //img.SetWidth(200);
-        //img.SetHeight(200);
-
-        // 添加图片和文字在同一段落中  
-        Paragraph imageParagraph = new Paragraph();
-        imageParagraph.Add(img);
-        imageParagraph.SetTextAlignment(TextAlignment.CENTER);
-        document.Add(imageParagraph);
-
-        // 关闭Document  
-        document.Close();
-    }
-
-    [Fact]
     public void Run()
     {
         string fontPath = Path.Combine(Environment.CurrentDirectory, "PDF", "simsun.ttf");
 
         byte[] imageContent = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "PDF", "sk.png"));
+        byte[] imageContent2 = File.ReadAllBytes(Path.Combine(Environment.CurrentDirectory, "PDF", "2.png"));
 
         if (fontPath == null)
         {
@@ -75,9 +34,10 @@ public class iTextWritePdfTest
                      "在这个示例中，我们创建了一个PdfWriter实例来指定文件的输出位置，创建了一个PdfDocument实例并添加了三页到PDF文档中。",
                      "这个示例展示了如何在PDF的指定页面上同时添加文本和图片。如果你有更多的需求，比如在不同的页面添加不同的内容或调整内容的位置和样式，你可以根据需要进一步修改代码。",
                      "希望这个示例对你有所帮助！如果你有更多问题，欢迎继续提问。"],
-                ImageContents = [imageContent]
+                ImageContents = [imageContent,imageContent2]
             },
         ];
+
 
         PdfWriter writer = new(outputPath);
 
@@ -99,22 +59,35 @@ public class iTextWritePdfTest
             {
                 Paragraph paragraph = new Paragraph(item)
                     .SetFont(font)
-                    .SetFontSize(12);
+                    .SetFontSize(10);
 
                 canvas.Add(paragraph);
             }
 
-            foreach (var item in page.ImageContents)
+            for (int i = 0; i < page.ImageContents.Count; i++)
             {
-                ImageData imageData = ImageDataFactory.Create(item);
+                ImageData imageData = ImageDataFactory.Create(page.ImageContents[i]);
 
                 Image image = new(imageData);
-                //image.SetWidth(pdf.GetDefaultPageSize().GetWidth() - 300);
-                //image.SetAutoScaleHeight(true);
-                image.ScaleToFit(pdf.GetDefaultPageSize().GetWidth(), pdf.GetDefaultPageSize().GetHeight());
 
-                canvas.Add(image);
+                //image.SetWidth(pdf.GetDefaultPageSize().GetWidth() - 300);
+
+                //image.SetFixedPosition(i + 1, 0, 0);
+
+                //canvas.Add(image);
             }
+
+            //foreach (var item in page.ImageContents)
+            //{
+            //    ImageData imageData = ImageDataFactory.Create(item);
+
+            //    Image image = new(imageData);
+            //    //image.SetWidth(pdf.GetDefaultPageSize().GetWidth() - 300);
+            //    //image.SetAutoScaleHeight(true);
+            //    image.ScaleToFit(pdf.GetDefaultPageSize().GetWidth(), pdf.GetDefaultPageSize().GetHeight());
+
+            //    canvas.Add(image);
+            //}
         }
 
         // 关闭文档  
---
+++ quickstarts/UnitTests/PDF/iTextWritePdfTest2.cs
@@ -0,0 +1,100 @@
+﻿using iText.IO.Font;
+using iText.IO.Image;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+//using iText.Layout.Borders;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace UnitTests.iText;
+
+public class iTextWritePdfTest2
+{
+
+    [Fact]
+    public static void Run()
+    {
+        string outputPath = "test_write2.pdf";
+
+        string fontPath = System.IO.Path.Combine(Environment.CurrentDirectory, "PDF", "simsun.ttf");
+
+        byte[] imageContent = File.ReadAllBytes(System.IO.Path.Combine(Environment.CurrentDirectory, "PDF", "sk.png"));
+        byte[] imageContent2 = File.ReadAllBytes(System.IO.Path.Combine(Environment.CurrentDirectory, "PDF", "2.png"));
+
+        List<PdfPageContent> pageContents =
+        [
+            new PdfPageContent
+            {
+                TextContents = [
+                     "在这个示例中，我们创建了一个PdfWriter实例来指定文件的输出位置，创建了一个PdfDocument实例并添加了三页到PDF文档中。",
+                     "这个示例展示了如何在PDF的指定页面上同时添加文本和图片。如果你有更多的需求，比如在不同的页面添加不同的内容或调整内容的位置和样式，你可以根据需要进一步修改代码。",
+                     "希望这个示例对你有所帮助！如果你有更多问题，欢迎继续提问。"],
+                ImageContents = [imageContent,imageContent2]
+            },
+        ];
+
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(outputPath));
+
+        pdfDoc.SetDefaultPageSize(PageSize.A4);
+
+        Document doc = new Document(pdfDoc);
+
+        PdfFont font = PdfFontFactory.CreateFont(fontPath, PdfEncodings.IDENTITY_H, pdfDoc);
+
+        foreach (PdfPageContent page in pageContents)
+        {
+            PageSize pageSize = pdfDoc.GetDefaultPageSize();
+
+            Table table = new Table(UnitValue.CreatePercentArray(1));
+
+            foreach (var item in page.TextContents)
+            {
+                table.AddCell(CreateTextCell(font, item));
+            }
+            table.AddCell(CreateTextCell(font, "一个新的文本内容1"));
+
+            foreach (var item in page.ImageContents)
+            {
+                table.AddCell(CreateImageCell(item, pageSize.GetWidth() * 0.5f));
+            }
+
+            table.AddCell(CreateTextCell(font, "一个新的文本内容2"));
+
+            doc.Add(table);
+        }
+
+        doc.Close();
+    }
+
+    private static Cell CreateImageCell(byte[] imageContent, float width)
+    {
+        Cell cell = new Cell();
+
+        Image image = new Image(ImageDataFactory.Create(imageContent));
+
+        image.SetWidth(width);
+        image.SetAutoScaleHeight(false);
+
+        cell.Add(image);
+
+        //cell.SetBorder(Border.NO_BORDER);
+
+        return cell;
+    }
+
+    private static Cell CreateTextCell(PdfFont font, string text)
+    {
+        Cell cell = new Cell();
+        Paragraph paragraph = new Paragraph(text)
+                    .SetFont(font)
+                    .SetFontSize(3);
+
+        cell.Add(paragraph);
+
+        //cell.SetBorder(Border.NO_BORDER);
+
+        return cell;
+    }
+}
