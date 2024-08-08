fdb3bcca3c879faa0553abcf144331f5dc14290c
ShuaiHua Du <shuaihua.du@outlook.com>
2024-08-09 01:48:35 +0800

    Add new password generator.

--- quickstarts/UnitTests/Security/PasswordGenerator_Test.cs
+++ quickstarts/UnitTests/Security/PasswordGenerator_Test.cs
@@ -1,16 +1,25 @@
-﻿
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace UnitTests.Security;
 
 public class PasswordGenerator_Test(ITestOutputHelper output) : BaseTest(output)
 {
-    public static string GeneratePassword(int length)
+    [Fact]
+    public void Run()
+    {
+        this.Output.WriteLine(GeneratePassword1(8));
+        this.Output.WriteLine(GeneratePassword2(8));
+        this.Output.WriteLine(GeneratePassword3(8));
+        this.Output.WriteLine(RandomPasswordGenerator.GeneratePassword(8));
+
+    }
+
+    public static string GeneratePassword1(int length)
     {
         const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
         StringBuilder result = new StringBuilder(length);
-        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+        using (RNGCryptoServiceProvider rng = new())
         {
             byte[] uintBuffer = new byte[sizeof(uint)];
 
@@ -22,5 +31,33 @@ public class PasswordGenerator_Test(ITestOutputHelper output) : BaseTest(output)
             }
         }
         return result.ToString();
+    }
+
+    public static string GeneratePassword2(int length)
+    {
+        const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+        StringBuilder result = new StringBuilder(length);
+        byte[] randomBytes = new byte[length];
+
+        using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+        {
+            rng.GetBytes(randomBytes);
+        }
+
+        foreach (byte b in randomBytes)
+        {
+            result.Append(validChars[b % validChars.Length]);
+        }
+
+        return result.ToString();
+    }
+
+    public static string GeneratePassword3(int length)
+    {
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+        var random = new Random();
+        string password = new string(Enumerable.Repeat(chars, length)
+          .Select(s => s[random.Next(s.Length)]).ToArray());
+        return password;
     }
 }
---
+++ quickstarts/UnitTests/Security/RandomPasswordGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace UnitTests.Security;
+
+public class RandomPasswordGenerator
+{
+    public static string GeneratePassword(int length)
+    {
+        const string upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+        const string digits = "0123456789";
+        const string specialChars = "!@#$%^&*()-_=+[]{}|;:,.<>?";
+
+        string allChars = upperCase + lowerCase + digits + specialChars;
+
+        StringBuilder password = new StringBuilder();
+
+        RandomNumberGenerator rng = RandomNumberGenerator.Create();
+
+        // 确保密码包含至少一个大写字母、一个小写字母、一个数字和一个特殊符号  
+        password.Append(upperCase[GetRandomNumber(rng, upperCase.Length)]);
+        password.Append(lowerCase[GetRandomNumber(rng, lowerCase.Length)]);
+        password.Append(digits[GetRandomNumber(rng, digits.Length)]);
+        password.Append(specialChars[GetRandomNumber(rng, specialChars.Length)]);
+
+        // 填充剩余的密码长度  
+        for (int i = 4; i < length; i++)
+        {
+            password.Append(allChars[GetRandomNumber(rng, allChars.Length)]);
+        }
+
+        // 打乱密码字符顺序  
+        return ShuffleString(password.ToString(), rng);
+    }
+
+    static int GetRandomNumber(RandomNumberGenerator rng, int max)
+    {
+        byte[] randomNumber = new byte[4];
+        rng.GetBytes(randomNumber);
+        int value = BitConverter.ToInt32(randomNumber, 0) & int.MaxValue;
+        return value % max;
+    }
+
+    static string ShuffleString(string input, RandomNumberGenerator rng)
+    {
+        char[] array = input.ToCharArray();
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = GetRandomNumber(rng, i + 1);
+            char temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return new string(array);
+    }
+}
