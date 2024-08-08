9be63ea2c6e92caf86f73c9c0ef744053ebaaf4b
ShuaiHua Du <shuaihua.du@outlook.com>
2024-08-08 23:07:22 +0800

    Add password generator.

---
+++ quickstarts/UnitTests/Security/PasswordGenerator_Test.cs
@@ -0,0 +1,26 @@
+﻿
+using System.Security.Cryptography;
+using System.Text;
+
+namespace UnitTests.Security;
+
+public class PasswordGenerator_Test(ITestOutputHelper output) : BaseTest(output)
+{
+    public static string GeneratePassword(int length)
+    {
+        const string validChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()";
+        StringBuilder result = new StringBuilder(length);
+        using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+        {
+            byte[] uintBuffer = new byte[sizeof(uint)];
+
+            while (length-- > 0)
+            {
+                rng.GetBytes(uintBuffer);
+                uint num = BitConverter.ToUInt32(uintBuffer, 0);
+                result.Append(validChars[(int)(num % (uint)validChars.Length)]);
+            }
+        }
+        return result.ToString();
+    }
+}
